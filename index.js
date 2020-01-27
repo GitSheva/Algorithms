c0fabf261d07faa3b425e82132accf9d512a4ac5
Unknown <aryabko@gmail.com>
2020-01-27 10:26:15 -0800

    adding reverse string

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let Greeting = (name) => {
 
     console.log("Hello " + name);
+
+    let map = new Map();
+
+    map["name"] = "Max";
+    map["name"] = "Igor";
+
+    console.log(map["name"]);
+    console.log(map["lastname"]);
 }
 
 Greeting("Artem");
